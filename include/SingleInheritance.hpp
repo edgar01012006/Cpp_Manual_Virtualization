42013d53b19b0269c2920fd6f285717e0ca23892
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-11-19 17:00:33 +0400

    updated header files

--- include/SingleInheritance.hpp
+++ include/SingleInheritance.hpp
@@ -6,19 +6,29 @@
 
 class Base : public Virtualizable {
     public:
-        Base(FnPtr fn1 = (FnPtr)&Base::foo, FnPtr fn2 = (FnPtr)&Base::bar) : Virtualizable{} {
+        Base(const std::string& typeId = getTypeId(), FnPtr fn1 = (FnPtr)&Base::foo, FnPtr fn2 = (FnPtr)&Base::bar) 
+            : Virtualizable{} {
+            setTypeId(typeId);
             registerVirtual("foo", fn1);
             registerVirtual("bar", fn2);
         }
+
         void foo() { std::cout << "Base foo\n"; }
         void bar() { std::cout << "Base bar\n"; }
+
+        static std::string getTypeId() { return "Base"; }
 };
 
 class Derived : public Base {
     public:
-        Derived() : Base ((FnPtr)&Derived::foo, (FnPtr)&Derived::bar) {} // no use case for "type name" for now
+        Derived(const std::string& typeId = "Derived") 
+            : Base (typeId, (FnPtr)&Derived::foo, (FnPtr)&Derived::bar) {}
+            
         void foo() { std::cout << "Derived foo\n"; }
+        void loo() { std::cout << "Derived loo\n"; }
+
         void bar() { std::cout << "Derived bar\n"; }
+        static std::string getTypeId() { return "Derived"; }
 };
 
 #endif /* SINGLEINHERITANCE_HPP */
--- include/Virtualizable.hpp
+++ include/Virtualizable.hpp
@@ -4,14 +4,31 @@
 #include <vector>
 #include <string>
 
+class Virtualizable;
+using FnPtr = void(Virtualizable::*)();
+
 class Virtualizable {
+    protected:
+        void setTypeId(const std::string& typeId) { m_typeId = typeId; }
+
     public:
-        using FnPtr = void(Virtualizable::*)();
         void registerVirtual(const std::string& name, FnPtr fn);
         void callVirtual(const std::string& name);
+    
+        std::string getTypeId() { return m_typeId; }
+    
     private:
+        std::string m_typeId;
         std::vector<std::string> m_names;
         std::vector<FnPtr> m_fns;
 };
+
+template <typename T>
+T* dynamicCast(Virtualizable* type) {
+    if (type->getTypeId() == T::getTypeId()) {
+        return static_cast<T*>(type);
+    }
+    return nullptr;
+}
 
 #endif /* VIRTUALIZABLE_HPP */
