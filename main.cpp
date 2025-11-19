d2e738753bf518026996487b90c94488867f2ca5
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-11-19 17:01:27 +0400

    updated main

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 #include "include/SingleInheritance.hpp"
 
 int main() {
+    /*
     Derived derivedObj;
     Base* ptr = &derivedObj;
     ptr->callVirtual("foo");
     ptr->callVirtual("bar");
     Base baseObj;
+
     baseObj.callVirtual("foo");
     baseObj.callVirtual("bar");
+    */
+
+    Virtualizable* v = new Derived;
+
+    if (Derived* d = dynamicCast<Derived>(v)) {
+         std::cout << "Cast is successful\n";
+    }
+    else { std::cout << "Case is unsuccessful\n"; }
+
+    if (Base* b = dynamicCast<Base>(v)) {
+        std::cout << "Cast is successful\n";
+    }
+    else { std::cout << "Case is unsuccessful\n"; }
 }
