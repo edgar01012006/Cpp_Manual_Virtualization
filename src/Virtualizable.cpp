e9d81175b4274310bb654e0a6aee3e1d8933da67
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-11-19 17:01:01 +0400

    updated source files

--- src/Virtualizable.cpp
+++ src/Virtualizable.cpp
@@ -2,14 +2,13 @@
 
 void Virtualizable::registerVirtual(const std::string& name, FnPtr fn) {
     m_names.push_back(name);
-    m_fns.push_back(fn); // if using is in global scope it doesnt work, but when its inside class it works why?
+    m_fns.push_back(fn);
 }
 
 void Virtualizable::callVirtual(const std::string& name) {
     for (int i = 0; i < m_names.size(); ++i) {
         if (m_names[i] == name) {
-            (this->*m_fns[i])(); // (*m_fns[i])() <-this doesnt work, but with "this->" it works why?;
-                                 // when workign with regular function pointers we dont dereference, so why use dereference here?
+            (this->*m_fns[i])();
         }
     }
 }
